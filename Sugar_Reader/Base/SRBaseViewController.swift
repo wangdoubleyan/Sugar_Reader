798f735edba8f1851d6b3ef79371d55a939c56d5
YY <wangdoubleyan@qq.com>
2019-07-03 18:21:51 +0800

    first commit

---
+++ Sugar_Reader/Base/SRBaseViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SRBaseViewController.swift
+//  Sugar_Reader
+//
+//  Created by YY on 2019/7/3.
+//  Copyright © 2019 Sugar. All rights reserved.
+//
+
+import UIKit
+
+class SRBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Sugar_Reader/Sugar_Reader.h
@@ -0,0 +1,19 @@
+//
+//  Sugar_Reader.h
+//  Sugar_Reader
+//
+//  Created by YY on 2019/7/3.
+//  Copyright © 2019 Sugar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Sugar_Reader.
+FOUNDATION_EXPORT double Sugar_ReaderVersionNumber;
+
+//! Project version string for Sugar_Reader.
+FOUNDATION_EXPORT const unsigned char Sugar_ReaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sugar_Reader/PublicHeader.h>
+
+
