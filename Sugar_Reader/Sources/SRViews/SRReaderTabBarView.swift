fc763dcac1ef55786e8be483b48096545012f58d
YY <wangdoubleyan@qq.com>
2019-07-03 18:47:00 +0800

    add VC

---
+++ Sugar_Reader/Sources/SRViews/SRReaderTabBarView.swift
@@ -0,0 +1,21 @@
+//
+//  SRReaderTabBarView.swift
+//  Sugar_Reader
+//
+//  Created by YY on 2019/7/3.
+//  Copyright © 2019 Sugar. All rights reserved.
+//
+
+import UIKit
+
+class SRReaderTabBarView: SRBaseView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Sugar_Reader/Sources/SRViews/SRReaserNavBarView.swift
@@ -0,0 +1,21 @@
+//
+//  SRReaserNavBarView.swift
+//  Sugar_Reader
+//
+//  Created by YY on 2019/7/3.
+//  Copyright © 2019 Sugar. All rights reserved.
+//
+
+import UIKit
+
+class SRReaserNavBarView: SRBaseView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
